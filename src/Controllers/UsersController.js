27231390e779d9948b509482b74e63298ff87f9f
giulia <giulia.guerreiro@gmail.com>
2022-08-17 18:21:13 -0300

    create users operation added

--- src/Controllers/UsersController.js
+++ src/Controllers/UsersController.js
@@ -1,15 +1,34 @@
+const sqliteConnection = require('../database/sqlite')
 const AppError = require('../utils/AppError')
+const { hash, compare } = require('bcryptjs')
 
 class UsersController {
   
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
     if(!email || !password) {
       throw new AppError('Email e senha são obrigatórios')
     }
 
-    return response.status(201).json({ name, email, password })
+    const database = await sqliteConnection()
+
+    // Verify if email is already used
+    const emailExists = await database.get("SELECT * FROM users WHERE email = ?", [email])
+    if(emailExists) {
+      throw new AppError('Este email já está em uso')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(`
+    INSERT INTO users
+    (name, email, password)
+    VALUES
+    (?, ?, ?)
+    `, [name, email, hashedPassword])
+
+    return response.status(201).json()
   }
 }
 
